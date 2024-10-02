f5294b2f0394810fbae2b6bd7a9a141d0b9e4ecb
Alexander Nekrasov <rostex2@yandex.ru>
2024-10-03 01:01:06 +0300

    add code for command line add readfile method

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,52 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
+        description = "Compares two configuration files and shows a difference.")
+class App implements Callable<String> {
+
+    @Parameters(index = "0", description = "path to first file")
+    private static String filePath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private String filePath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
+
+    @Override
+    public String call() throws Exception { // your business logic goes here...
+        readfile(filePath1, filePath2);
+        return null;
+    }
+
+    public void readfile(String ... filepath) {
+        var filepaths = List.of(filepath);
+        filepaths.forEach((value) -> {
+            Path path = Paths.get(value).toAbsolutePath().normalize();
+            try {
+                System.out.println(Files.readString(path));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
