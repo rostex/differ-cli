d5e7cc7d4457214c50835c7cdbc493f6bf58661a
rostex <rostex2@yandex.ru>
2024-10-05 21:38:53 +0300

    Remove getData method Remove comments code

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,11 +5,8 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.File;
-import java.util.Map;
 import java.util.concurrent.Callable;
+import static hexlet.code.JsonParse.getData;
 
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
@@ -28,47 +25,8 @@ class App implements Callable<String> {
     @Override
     public String call() throws Exception { // your business logic goes here...
         System.out.println(JsonParse.getDiff(getData(filePath1), getData(filePath2)));
-        //System.out.println(getData(filePath1));
         return null;
     }
-
-    /*
-
-    private void readfile(String ... filepath) {
-        var filepaths = List.of(filepath);
-        filepaths.forEach((value) -> {
-            Path path = Paths.get(value).toAbsolutePath().normalize();
-            try {
-                System.out.println(Files.readString(path));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-
-     */
-
-//    public static void getData(String ... filePath) throws Exception {
-//        var filePaths = List.of(filePath);
-//
-//        filePaths.forEach((value) -> {
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        File file = new File(value);
-//            try {
-//                Map<String, String> = objectMapper.readValue(file, Server.class);
-//            } catch (Exception e) {
-//                throw new RuntimeException(e);
-//            }
-//        });
-//    }
-
-    public static Map getData(String filePath) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        File file = new File(filePath);
-        return objectMapper.readValue(file, Map.class);
-
-    }
-
 
     public static void main(String[] args) throws Exception {
         int exitCode = new CommandLine(new App()).execute(args);
