0a1e1e1b09b827d55771ba2a78e2384ce010c8a4
Alexander Nekrasov <rostex2@yandex.ru>
2024-10-06 18:39:53 +0300

    Update AppTest.java

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -7,11 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
 
-    String filePath1 = "src/test/resources/testfile1.json";
-    String filePath2 = "src/test/resources/testfile2.json";
+    String fileJsonPath1 = "src/test/resources/testfile1.json";
+    String fileJsonPath2 = "src/test/resources/testfile2.json";
+    String fileYamlPath1 = "src/test/resources/testfile1.yml";
+    String fileYamlPath2 = "src/test/resources/testfile2.yml";
 
     @Test
-    public void testCall() throws Exception {
+    public void testCallJson() throws Exception {
         assertEquals("{"
                         + "\n"
                         + "  application: JsonDiff" + "\n"
@@ -22,7 +24,23 @@ public class AppTest {
                         + "+ version: 5"
                         + "\n"
                         + "}",
-                JsonParse.getDiff(getData(filePath1), getData(filePath2)));
+                JsonParse.getDiff(getData(fileJsonPath1), getData(fileJsonPath2)));
+
+    }
+
+    @Test
+    public void testCallYaml() throws Exception {
+        assertEquals("{"
+                        + "\n"
+                        + "  application: JsonDiff" + "\n"
+                        + "- client: windows" + "\n"
+                        + "+ client: macOS" + "\n"
+                        + "- open-source: true" + "\n"
+                        + "- version: 2" + "\n"
+                        + "+ version: 5"
+                        + "\n"
+                        + "}",
+                JsonParse.getDiff(getData(fileYamlPath1), getData(fileYamlPath2)));
 
     }
 }
