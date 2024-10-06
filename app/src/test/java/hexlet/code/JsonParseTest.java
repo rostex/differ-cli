4e5b3a1c175722a3f645ed22bfd6997333386c5e
Alexander Nekrasov <rostex2@yandex.ru>
2024-10-06 18:40:14 +0300

    Update JsonParseTest.java

--- app/src/test/java/hexlet/code/JsonParseTest.java
+++ app/src/test/java/hexlet/code/JsonParseTest.java
@@ -1,18 +1,24 @@
 package hexlet.code;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class JsonParseTest {
     Map<String, Object> content1 = new HashMap<>();
     Map<String, Object> content2 = new HashMap<>();
 
+    Map<String, Object> content3 = new HashMap<>();
+    Map<String, Object> content4 = new HashMap<>();
+
     @BeforeEach
-    public void beforeEach () {
+    public void beforeEach() {
         content1.put("client", "windows");
         content1.put("open-source", true);
         content1.put("version", 2);
@@ -21,21 +27,82 @@ public class JsonParseTest {
         content2.put("application", "JsonDiff");
         content2.put("version", 5);
         content2.put("client", "macOS");
+
+        content3.put("setting1", "Some value");
+        content3.put("setting2", 200);
+        content3.put("setting3", true);
+        content3.put("key1", "value1");
+        //content3.put("numbers1", [1, 2, 3, 4]);
+        //content3.put("numbers2", [2, 3, 4, 5]);
+        content3.put("id", null);
+        content3.put("default", false);
+        content3.put("checked", 45);
+        //content3.put("numbers3", [3, 4, 5]);
+        //content3.put("chars1", ["a", "b", "c"]);
+        //content3.put("chars2", ["d", "e", "f"]);
+
+        content4.put("setting1", "Another value");
+        content4.put("setting2", 300);
+        content4.put("setting3", "none");
+        content4.put("key2", "value2");
+        //content4.put("numbers1", [1, 2, 3, 4]);
+        //content4.put("numbers2", [22, 33, 44, 55]);
+        content4.put("id", null);
+        //content4.put("default", ["value1", "value2"]);
+        content4.put("checked", true);
+        //content4.put("numbers4", [4, 5, 6]);
+        //content4.put("chars1", ["a", "b", "c"]);
+        content4.put("chars2", false);
+        //content4.put("obj1", {"nestedKey": "value", "isNested": true}
+
     }
 
     @Test
     public void testGetDiff() {
         assertEquals("{"
-                + "\n"
-                + "  application: JsonDiff" + "\n"
-                + "- client: windows" + "\n"
-                + "+ client: macOS" + "\n"
-                + "- open-source: true" + "\n"
-                + "- version: 2" + "\n"
-                + "+ version: 5"
-                + "\n"
-                + "}",
+                        + "\n"
+                        + "  application: JsonDiff" + "\n"
+                        + "- client: windows" + "\n"
+                        + "+ client: macOS" + "\n"
+                        + "- open-source: true" + "\n"
+                        + "- version: 2" + "\n"
+                        + "+ version: 5"
+                        + "\n"
+                        + "}",
                 JsonParse.getDiff(content1, content2));
+
+    }
+
+    @Test
+    public void testGetDiffMulti() {
+        assertEquals("{"
+                        + "\n"
+                        + "chars1: [a, b, c]" + "\n"
+                        + "- chars2: [d, e, f]" + "\n"
+                        + "+ chars2: false" + "\n"
+                        + "- checked: false" + "\n"
+                        + "+ checked: true" + "\n"
+                        + "- default: null" + "\n"
+                        + "+ default: [value1, value2]" + "\n"
+                        + "- id: 45" + "\n"
+                        + "+ id: null" + "\n"
+                        + "- key1: value1" + "\n"
+                        + "+ key2: value2" + "\n"
+                        + "numbers1: [1, 2, 3, 4]" + "\n"
+                        + "- numbers2: [2, 3, 4, 5]" + "\n"
+                        + "+ numbers2: [22, 33, 44, 55]" + "\n"
+                        + "- numbers3: [3, 4, 5]" + "\n"
+                        + "+ numbers4: [4, 5, 6]" + "\n"
+                        + "+ obj1: {nestedKey=value, isNested=true}" + "\n"
+                        + "- setting1: Some value" + "\n"
+                        + "+ setting1: Another value" + "\n"
+                        + "- setting2: 200" + "\n"
+                        + "+ setting2: 300" + "\n"
+                        + "- setting3: true" + "\n"
+                        + "+ setting3: none"
+                        + "\n"
+                        + "}",
+                JsonParse.getDiff(content3, content4));
 
     }
 }
