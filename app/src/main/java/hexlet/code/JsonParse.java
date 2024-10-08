52267147831dbf0516b08a7499634925484ead1c
Alexander Nekrasov <rostex2@yandex.ru>
2024-10-08 21:27:24 +0300

    Update code

--- app/src/main/java/hexlet/code/JsonParse.java
+++ app/src/main/java/hexlet/code/JsonParse.java
@@ -4,62 +4,64 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.io.File;
-import java.util.Map;
+import java.util.*;
+import hexlet.code.Data;
+
+
 
 public class JsonParse {
 
     public static String getDiff(Map<String, Object> content1, Map<String, Object> content2) {
         StringBuilder result = new StringBuilder();
 
-        var sortedContent1 = content1.entrySet().stream()
-                .sorted(Map.Entry.comparingByKey())
-                .toList();
+        Set<String> sortedKeys = new TreeSet<>();
+        sortedKeys.addAll(content1.keySet());
+        sortedKeys.addAll(content2.keySet());
 
-        var sortedContent2 = content2.entrySet().stream()
-                .sorted(Map.Entry.comparingByKey())
-                .toList();
+        Data data = new Data
 
-        result.append("{").append("\n");
-        for (var entry : sortedContent1) {
-            var key = entry.getKey();
-            var value1 = entry.getValue();
+        for (var key : sortedKeys) {
+            var value1 = content1.get(key);
             var value2 = content2.get(key);
 
-            if (!content2.containsKey(key)) {
+            if (value1 == null && value2 == null) {
+                result.append("  ").append(key).append(": ").append(value1).append("\n");
+            } else if (value1 == null) {
+                result.append("+ ").append(key).append(": ").append(value2).append("\n");
+            } else if (value2 == null) {
                 result.append("- ").append(key).append(": ").append(value1).append("\n");
-            } else if (!value1.equals(value2)) {
+            }else if (!value1.equals(value2)) {
                 result.append("- ").append(key).append(": ").append(value1).append("\n");
                 result.append("+ ").append(key).append(": ").append(value2).append("\n");
             } else {
                 result.append("  ").append(key).append(": ").append(value1).append("\n");
             }
         }
-
-        for (var entry : sortedContent2) {
-            var key = entry.getKey();
-            if (!content1.containsKey(key)) {
-                result.append("+ ").append(key).append(": ").append(entry.getValue()).append("\n");
-            }
-        }
         result.append("}");
+
         return result.toString();
     }
 
     public static Map getData(String filePath) throws Exception {
-        int indexOfDoT = filePath.indexOf(".");
-        var fileFormat = filePath.substring(indexOfDoT + 1);
-
-        if (fileFormat.equals("yml") || fileFormat.equals("yaml")) {
+        if (getFileFormat(filePath).equals("yml") || getFileFormat(filePath).equals("yaml")) {
             YAMLMapper yamlMapper = new YAMLMapper();
             File file = new File(filePath);
             return yamlMapper.readValue(file, Map.class);
-        } else {
+        } else if (getFileFormat(filePath).equals("json")){
             ObjectMapper objectMapper = new ObjectMapper();
             File file = new File(filePath);
             return objectMapper.readValue(file, Map.class);
+        } else {
+            throw new Exception(filePath + " format is not supported");
         }
 
     }
+
+    private static String getFileFormat (String filePath) {
+        int indexOfDoT = filePath.indexOf(".");
+        return filePath.substring(indexOfDoT + 1);
+    }
+
 
 }
 
