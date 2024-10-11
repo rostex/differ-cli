1b3f26336080894270de53d571e29eda98d8e33d
Alexander Nekrasov <rostex2@yandex.ru>
2024-10-11 19:24:56 +0300

    Fix code

--- app/src/test/java/hexlet/code/JsonParseTest.java
+++ app/src/test/java/hexlet/code/JsonParseTest.java
@@ -52,9 +52,9 @@ public class JsonParseTest {
         content4.put("chars1", new String[]{"a", "b", "c"});
         content4.put("chars2", false);
         content4.put("obj1", new HashMap<String, Object>() {{
-            put("nestedKey", "value");
-            put("isNested", true);
-        }});
+                put("nestedKey", "value");
+                put("isNested", true);
+            }});
     }
 
     @Test
