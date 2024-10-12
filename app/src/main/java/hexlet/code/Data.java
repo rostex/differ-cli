3be0127b8aab7f2e6f17bc917056092f4170d01e
rostex <rostex2@yandex.ru>
2024-10-12 17:36:02 +0300

    Override toString, equals and hashCode

--- app/src/main/java/hexlet/code/Data.java
+++ app/src/main/java/hexlet/code/Data.java
@@ -1,8 +1,12 @@
 package hexlet.code;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+@ToString
+@EqualsAndHashCode
 @Setter
 @Getter
 public class Data {
