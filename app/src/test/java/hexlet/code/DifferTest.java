6f3c22e7e98b076582114588d1a08884a6011b81
Alexander Nekrasov <rostex2@yandex.ru>
2024-10-15 00:20:31 +0300

    Update tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -3,79 +3,55 @@ package hexlet.code;
 import static hexlet.code.Differ.getFileFormat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 
 public class DifferTest {
-    String fileJsonPath1 = "src/test/resources/testfile1.json";
-    String fileJsonPath2 = "src/test/resources/testfile2.json";
-    String fileYamlPath1 = "src/test/resources/testfile1.yml";
-    String fileYamlPath2 = "src/test/resources/testfile2.yml";
+    private String jsonFile1 = "src/test/resources/testfile1.json";
+    private String jsonFile2 = "src/test/resources/testfile2.json";
+    private String yamlFile1 = "src/test/resources/testfile1.yml";
+    private String yamlFile2 = "src/test/resources/testfile2.yml";
 
-    Map<String, Object> content1 = new HashMap<>();
-    Map<String, Object> content2 = new HashMap<>();
+    private String expectedStylishFile = "src/test/resources/expected-stylish";
+    private String expectedPlainFile = "src/test/resources/expected-plain";
+    private String expectedJsonFile = "src/test/resources/expected-json";
 
-    Map<String, Object> content3 = new HashMap<>();
-    Map<String, Object> content4 = new HashMap<>();
 
-    List<Data> expected = new ArrayList<>();
-
-    @BeforeEach
-    public void beforeEach() {
-        content1.put("client", "windows");
-        content1.put("open-source", true);
-        content1.put("version", 2);
-        content1.put("application", "JsonDiff");
-
-        content2.put("application", "JsonDiff");
-        content2.put("version", 5);
-        content2.put("client", "macOS");
-/*
-        content3.put("setting1", "Some value");
-        content3.put("setting2", 200);
-        content3.put("setting3", true);
-        content3.put("key1", "value1");
-        content3.put("numbers1", new Integer[]{1, 2, 3, 4});
-        content3.put("numbers2", new Integer[]{2, 3, 4, 5});
-        content3.put("id", null);
-        content3.put("default", false);
-        content3.put("checked", 45);
-        content3.put("numbers3", new Integer[]{3, 4, 5});
-        content3.put("chars1", new String[]{"a", "b", "c"});
-        content3.put("chars2", new String[]{"d", "e", "f"});
-
-        content4.put("setting1", "Another value");
-        content4.put("setting2", 300);
-        content4.put("setting3", "none");
-        content4.put("key2", "value2");
-        content4.put("numbers1", new Integer[]{1, 2, 3, 4});
-        content4.put("numbers2", new Integer[]{22, 33, 44, 55});
-        content4.put("id", null);
-        content4.put("default", new String[]{"value1", "value2"});
-        content4.put("checked", true);
-        content4.put("numbers4", new Integer[]{4, 5, 6});
-        content4.put("chars1", new String[]{"a", "b", "c"});
-        content4.put("chars2", false);
-        content4.put("obj1", new HashMap<String, Object>() {{
-                put("nestedKey", "value");
-                put("isNested", true);
-            }});
-*/
-        expected.add(new Data("application", "JsonDiff", "JsonDiff", Data.Status.UNCHANGED));
-        expected.add(new Data("client", "windows", "macOS", Data.Status.UPDATED));
-        expected.add(new Data("open-source", true, null, Data.Status.REMOVED));
-        expected.add(new Data("version", 2, 5, Data.Status.UPDATED));
+    private static String readFixture(String fileName) throws Exception {
+        return Files.readString(Path.of(fileName));
     }
 
     @Test
-    public void testGetDiff() {
-        assertEquals(expected, Differ.getDiff(content1, content2));
+    public void testJsonToStylishDifferGenerate() throws Exception {
+        assertEquals(readFixture(expectedStylishFile), Differ.generate(jsonFile1, jsonFile2));
+    }
 
+    @Test
+    public void testYamlToStylishDifferGenerate() throws Exception {
+        assertEquals(readFixture(expectedStylishFile), Differ.generate(yamlFile1, yamlFile2));
+    }
+
+    @Test
+    public void testJsonToPlainDifferGenerate() throws Exception {
+        assertEquals(readFixture(expectedPlainFile), Differ.generate(jsonFile1, jsonFile2, "plain"));
+    }
+
+    @Test
+    public void testYamlToPlainDifferGenerate() throws Exception {
+        assertEquals(readFixture(expectedPlainFile), Differ.generate(yamlFile1, yamlFile2, "plain"));
+    }
+
+    @Test
+    public void testJsonToJsonDifferGenerate() throws Exception {
+        assertEquals(readFixture(expectedJsonFile), Differ.generate(jsonFile1, jsonFile2, "json"));
+    }
+
+    @Test
+    public void testYamlToJsonDifferGenerate() throws Exception {
+        assertEquals(readFixture(expectedJsonFile), Differ.generate(yamlFile1, yamlFile2, "json"));
     }
 
     @Test
@@ -84,20 +60,5 @@ public class DifferTest {
         assertEquals("json", getFileFormat("file1.my-lib.2024.json"));
     }
 
-    @Test
-    public void testGenerate() throws Exception {
-        assertEquals("{"
-                        + "\n"
-                        + "  application: JsonDiff" + "\n"
-                        + "- client: windows" + "\n"
-                        + "+ client: macOS" + "\n"
-                        + "- open-source: true" + "\n"
-                        + "- version: 2" + "\n"
-                        + "+ version: 5"
-                        + "\n"
-                        + "}",
-                Differ.generate(fileJsonPath1, fileJsonPath2, "stylish"));
-
-    }
 
 }
--- app/src/test/java/hexlet/code/FormatterTest.java
+++ app/src/test/java/hexlet/code/FormatterTest.java
@@ -3,19 +3,16 @@ package hexlet.code;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static hexlet.code.Formatter.checkValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class FormatterTest {
-    String content1;
-    Integer[] content2;
-    Map<String, Object> content3 = new HashMap<>();
-    List<Data> content4 = new ArrayList<>();
+class FormatterTest {
+    private String content1;
+    private Integer[] content2;
+    private Map<String, Object> content3 = new HashMap<>();
 
     @BeforeEach
     public void beforeEach() {
@@ -23,79 +20,6 @@ public class FormatterTest {
         content2 = new Integer[]{4, 5, 6};
         content3.put("nestedKey", "value");
         content3.put("isNested", true);
-
-        content4.add(new Data("chars1", new String[]{"a", "b", "c"}, new String[]{"a", "b", "c"},
-                Data.Status.UNCHANGED));
-        content4.add(new Data("chars2", new String[]{"d", "e", "f"}, false, Data.Status.UPDATED));
-        content4.add(new Data("checked", false, true, Data.Status.UPDATED));
-        content4.add(new Data("default", null, new String[]{"value1", "value2"}, Data.Status.UPDATED));
-        content4.add(new Data("id", 45, null, Data.Status.UPDATED));
-        content4.add(new Data("key1", "value1", null, Data.Status.REMOVED));
-        content4.add(new Data("key2", null, "value2", Data.Status.ADDED));
-        content4.add(new Data("numbers1", new Integer[]{1, 2, 3, 4}, new Integer[]{1, 2, 3, 4},
-                Data.Status.UNCHANGED));
-        content4.add(new Data("numbers2", new Integer[]{2, 3, 4, 5}, new Integer[]{22, 33, 44, 55},
-                Data.Status.UPDATED));
-        content4.add(new Data("numbers3", new Integer[]{3, 4, 5}, null, Data.Status.REMOVED));
-        content4.add(new Data("numbers4", null, new Integer[]{4, 5, 6}, Data.Status.ADDED));
-        content4.add(new Data("obj1", null, new HashMap<String, Object>() {{
-                put("nestedKey", "value");
-                put("isNested", true);
-            }}, Data.Status.ADDED));
-        content4.add(new Data("setting1", "Some value", "Another value", Data.Status.UPDATED));
-        content4.add(new Data("setting2", 200, 300, Data.Status.UPDATED));
-        content4.add(new Data("setting3", true, "none", Data.Status.UPDATED));
-    }
-
-    /*
-@Test
-public void testStylish() {
-    assertEquals("{"
-                    + "\n"
-                    + "chars1: [a, b, c]" + "\n"
-                    + "- chars2: [d, e, f]" + "\n"
-                    + "+ chars2: false" + "\n"
-                    + "- checked: false" + "\n"
-                    + "+ checked: true" + "\n"
-                    + "- default: null" + "\n"
-                    + "+ default: [value1, value2]" + "\n"
-                    + "- id: 45" + "\n"
-                    + "+ id: null" + "\n"
-                    + "- key1: value1" + "\n"
-                    + "+ key2: value2" + "\n"
-                    + "numbers1: [1, 2, 3, 4]" + "\n"
-                    + "- numbers2: [2, 3, 4, 5]" + "\n"
-                    + "+ numbers2: [22, 33, 44, 55]" + "\n"
-                    + "- numbers3: [3, 4, 5]" + "\n"
-                    + "+ numbers4: [4, 5, 6]" + "\n"
-                    + "+ obj1: {nestedKey=value, isNested=true}" + "\n"
-                    + "- setting1: Some value" + "\n"
-                    + "+ setting1: Another value" + "\n"
-                    + "- setting2: 200" + "\n"
-                    + "+ setting2: 300" + "\n"
-                    + "- setting3: true" + "\n"
-                    + "+ setting3: none"
-                    + "\n"
-                    + "}",
-            Formatter.stylish(content4));
-
-}
-    */
-    @Test
-    public void testPlain() {
-        assertEquals("Property 'chars2' was updated. From [complex value] to false" + "\n"
-                + "Property 'checked' was updated. From false to true" + "\n"
-                + "Property 'default' was updated. From null to [complex value]" + "\n"
-                + "Property 'id' was updated. From 45 to null" + "\n"
-                + "Property 'key1' was removed" + "\n"
-                + "Property 'key2' was added with value: 'value2'" + "\n"
-                + "Property 'numbers2' was updated. From [complex value] to [complex value]" + "\n"
-                + "Property 'numbers3' was removed" + "\n"
-                + "Property 'numbers4' was added with value: [complex value]" + "\n"
-                + "Property 'obj1' was added with value: [complex value]" + "\n"
-                + "Property 'setting1' was updated. From 'Some value' to 'Another value'" + "\n"
-                + "Property 'setting2' was updated. From 200 to 300" + "\n"
-                + "Property 'setting3' was updated. From true to 'none'" + "\n", Formatter.plain(content4));
     }
 
     @Test
