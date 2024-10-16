fd54226e83be8007615606d941ca4f68e4e40f8f
Alexander Nekrasov <rostex2@yandex.ru>
2024-10-17 00:29:09 +0300

    Fixed tests when the "\r" char is added when reading a file in Windows OS

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -23,53 +23,57 @@ public class DifferTest {
     private static String readFixture(String fileName) throws Exception {
         return Files.readString(Path.of(fileName));
     }
+    
+    private void assertStringsEquals(String value1, String value2) {
+        assertEquals(value1.replaceAll("\r", ""), value2.replaceAll("\r", ""));
+    }
 
     @Test
     public void testJsonToStylishDifferGenerate() throws Exception {
         var expected = readFixture(expectedStylishFile);
         var actual = Differ.generate(jsonFile1, jsonFile2);
-        assertEquals(expected, actual);
+        assertStringsEquals(expected, actual);
     }
 
     @Test
     public void testYamlToStylishDifferGenerate() throws Exception {
         var expected = readFixture(expectedStylishFile);
         var actual = Differ.generate(yamlFile1, yamlFile2);
-        assertEquals(expected, actual);
+        assertStringsEquals(expected, actual);
     }
 
     @Test
     public void testJsonToPlainDifferGenerate() throws Exception {
         var expected = readFixture(expectedPlainFile);
         var actual = Differ.generate(jsonFile1, jsonFile2, "plain");
-        assertEquals(expected, actual);
+        assertStringsEquals(expected, actual);
     }
 
     @Test
     public void testYamlToPlainDifferGenerate() throws Exception {
         var expected = readFixture(expectedPlainFile);
         var actual = Differ.generate(yamlFile1, yamlFile2, "plain");
-        assertEquals(expected, actual);
+        assertStringsEquals(expected, actual);
     }
 
     @Test
     public void testJsonToJsonDifferGenerate() throws Exception {
         var expected = readFixture(expectedJsonFile);
         var actual = Differ.generate(jsonFile1, jsonFile2, "json");
-        assertEquals(expected, actual);
+        assertStringsEquals(expected, actual);
     }
 
     @Test
     public void testYamlToJsonDifferGenerate() throws Exception {
         var expected = readFixture(expectedJsonFile);
         var actual = Differ.generate(yamlFile1, yamlFile2, "json");
-        assertEquals(expected, actual);
+        assertStringsEquals(expected, actual);
     }
 
     @Test
     public void testGetFileFormat() {
-        assertEquals("yml", getFileFormat("file1.yml"));
-        assertEquals("json", getFileFormat("file1.my-lib.2024.json"));
+        assertStringsEquals("yml", getFileFormat("file1.yml"));
+        assertStringsEquals("json", getFileFormat("file1.my-lib.2024.json"));
     }
 
 
