15d125880b26f9a12cff034387f63d2eefdda730
Alexander Nekrasov <rostex2@yandex.ru>
2024-10-19 00:14:55 +0300

    Remove Format enum from App.java
    Rewrite code without Format enum.
    Correct some texts

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,5 @@
 package hexlet.code;
 
-import hexlet.code.formatter.Format;
-import hexlet.code.formatter.Formatter;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -10,24 +8,24 @@ import picocli.CommandLine.Parameters;
 import java.util.concurrent.Callable;
 
 
-@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.8",
         description = "Compares two configuration files and shows a difference.")
 class App implements Callable<Integer> {
 
-    @Parameters(index = "0", description = "path to first file")
+    @Parameters(index = "0", description = "Path to first file")
     private static String filePath1;
 
-    @Parameters(index = "1", description = "path to second file")
+    @Parameters(index = "1", description = "Path to second file")
     private String filePath2;
 
-    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
-    private String stringFormat = "stylish";
+    @Option(names = {"-f", "--format"}, description = "Output format."
+            + " Available: stylish, plain, json [Default: stylish]")
+    private String format = "stylish";
 
     @Override
     public Integer call() throws Exception {
-        Format format = Formatter.formatConverter(stringFormat);
         System.out.println(Differ.generate(filePath1, filePath2, format));
-        return 0;
+        return 1;
     }
 
     public static void main(String[] args) throws Exception {
