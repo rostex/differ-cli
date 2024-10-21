04981c0bef64258054023bfef34ae49ba1332d44
rostex <rostex2@yandex.ru>
2024-10-21 11:38:47 +0300

    Create Format.java class with constant Strings

---
+++ app/src/main/java/hexlet/code/formatter/Format.java
@@ -0,0 +1,7 @@
+package hexlet.code.formatter;
+
+public class Format {
+    public static final String STYLISH = "stylish";
+    public static final String PLAIN = "plain";
+    public static final String JSON = "json";
+}
