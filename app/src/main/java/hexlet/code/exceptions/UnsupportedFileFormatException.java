60134bb4dfa7897a29e9f7366abfa5b2420dd7a1
rostex <rostex2@yandex.ru>
2024-11-14 23:20:06 +0300

    Adjust description

--- app/src/main/java/hexlet/code/exceptions/UnsupportedFileFormatException.java
+++ app/src/main/java/hexlet/code/exceptions/UnsupportedFileFormatException.java
@@ -2,6 +2,6 @@ package hexlet.code.exceptions;
 
 public class UnsupportedFileFormatException extends Exception {
     public UnsupportedFileFormatException(String fileName) {
-        super("Unsupported dile getFormat: " + fileName);
+        super("Unsupported file extension: " + fileName);
     }
 }
